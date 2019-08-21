650cca7db6bfa5bdbbc5576c682e2492b75f1047
Dongxn@NUDT <Dong.Xiaoning@outlook.com>
2019-08-21 09:59:18 +0800

    format some unformatted code using yapf/autopep8

--- LinearRegression/linearregression_with_GDsolver.py
+++ LinearRegression/linearregression_with_GDsolver.py
@@ -12,12 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 def getData(dataSet):
     m, n = np.shape(dataSet)
     trainData = np.ones((m, n))
-    trainData[:,:-1] = dataSet[:,:-1]
-    trainLabel = dataSet[:,-1]
+    trainData[:, :-1] = dataSet[:, :-1]
+    trainLabel = dataSet[:, -1]
     return trainData, trainLabel
+
 
 def GradientDescent(x, y, theta, alpha, m, maxIterations):
     xTrans = x.transpose()
@@ -30,12 +32,14 @@ def GradientDescent(x, y, theta, alpha, m, maxIterations):
         print(gradient)
     return theta
 
+
 def predict(x, theta):
     m, n = np.shape(x)
-    xTest = np.ones((m, n+1))
+    xTest = np.ones((m, n + 1))
     xTest[:, :-1] = x
     yP = np.dot(xTest, theta)
     return yP
+
 
 dataPath = r"C:\Users\DongXiaoning\Downloads\Algorithms in Machine Learning Foundation and Techniques\LinearRegression\house.csv"
 dataSet = genfromtxt(dataPath, delimiter=',')
@@ -51,18 +55,16 @@ print(predict(x, theta))
 print('\n')
 
 #visualization
-fig=plt.figure()
-ax=fig.gca(projection='3d')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 ax.set_xlabel('x0 axis')
 ax.set_ylabel('x1 axis')
 ax.set_zlabel('y axis')
-ax.scatter(trainData[:,0],trainData[:,1],trainLabel,c='r')
+ax.scatter(trainData[:, 0], trainData[:, 1], trainLabel, c='r')
 
-x0=np.linspace(-2,5,20)
-x1=np.linspace(0,7,20)
-X0,X1=np.meshgrid(x0,x1)
-y=X0*theta[0]+X1*theta[1]+theta[2]
-ax.plot_wireframe(X0,X1,y,cstride=1,rstride=1,color='b')
+x0 = np.linspace(-2, 5, 20)
+x1 = np.linspace(0, 7, 20)
+X0, X1 = np.meshgrid(x0, x1)
+y = X0 * theta[0] + X1 * theta[1] + theta[2]
+ax.plot_wireframe(X0, X1, y, cstride=1, rstride=1, color='b')
 plt.show()
-
-
--- NonLinearRegression/PolynomialRegression/nonlinearregression.py
+++ NonLinearRegression/PolynomialRegression/nonlinearregression.py
@@ -4,46 +4,43 @@ Created on Tue Aug 20 17:34:26 2019
 
 @author: DongXiaoning
 """
+#console: autopep8 --in-place --aggressive --aggressive nonlinearregression.py
+#console: yapf -h
 
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 from sklearn.datasets import load_diabetes
 
+
 def nonlinearTransform(rawtrainData):
-    arr1=rawtrainData[:,:]
-    arr2=np.multiply(rawtrainData[:,2:5],rawtrainData[:,3:6])# x3x4,x4x5,x5x6
-    arr3=np.multiply(rawtrainData[:,0],rawtrainData[:,0])    # x1^2
-    arr3=arr3.reshape(len(arr3),1)                           # convert vector to matrix. ndarray: scalar, vector, matrix
-    arr4=np.multiply(rawtrainData[:,2],rawtrainData[:,2])    # x3^2
-    arr4=arr4.reshape(len(arr4),1)                           # convert vector to matrix
-    trainData=np.concatenate((arr1,arr2,arr3,arr4),axis=1)
+    arr1 = rawtrainData[:, :]
+    arr2 = np.multiply(rawtrainData[:, 2:5],
+                       rawtrainData[:, 3:6])  # x3x4,x4x5,x5x6
+    arr3 = np.multiply(rawtrainData[:, 0], rawtrainData[:, 0])    # x1^2
+    # convert vector to matrix. ndarray: scalar, vector, matrix
+    arr3 = arr3.reshape(len(arr3), 1)
+    arr4 = np.multiply(rawtrainData[:, 2], rawtrainData[:, 2])    # x3^2
+    # convert vector to matrix
+    arr4 = arr4.reshape(len(arr4), 1)
+    trainData = np.concatenate((arr1, arr2, arr3, arr4), axis=1)
     return trainData
 
-#generate dataset from scikit-learn instead of using local files. 
-#It is more convenient because it avoid reading local file to our program
+
+# generate dataset from scikit-learn instead of using local files.
+# It is more convenient because it avoid reading local file to our program
 boston_dataset = load_boston()
 
 diabetes = load_diabetes()
 print(diabetes.keys())
 print(diabetes['feature_names'])
-rawtrainData=diabetes['data'][:-100,:6] #To be simple, we only use first 6 features, index 0-5
-rawtrainLabel=diabetes['target'][:-100]
-rawtestData=diabetes['data'][-100:,:6]
-rawtestLabel=diabetes['target'][-100:]
+# To be simple, we only use first 6 features, index 0-5
+rawtrainData = diabetes['data'][:-100, :6]
+rawtrainLabel = diabetes['target'][:-100]
+rawtestData = diabetes['data'][-100:, :6]
+rawtestLabel = diabetes['target'][-100:]
 
-trainData=nonlinearTransform(rawtrainData)
-trainLabel=rawtrainLabel
-testData=nonlinearTransform(rawtestData)
-testLabel=rawtestLabel
-
-
-
-
-
-
-
-
-
-
-
+trainData = nonlinearTransform(rawtrainData)
+trainLabel = rawtrainLabel
+testData = nonlinearTransform(rawtestData)
+testLabel = rawtestLabel
