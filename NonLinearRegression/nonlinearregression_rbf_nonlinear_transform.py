dcfc84b99d9a9c7aac84b05247c558c09008d151
Dongxn@NUDT <Dong.Xiaoning@outlook.com>
2019-08-22 09:36:25 +0800

    add linear/nonlinear regression with regularization

---
+++ NonLinearRegression/nonlinearregression_rbf_nonlinear_transform.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 08:59:52 2019
+
+@author: DongXiaoning
+"""
+
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
---
+++ RegressionWithRegularization/nonlinearregression_with_Lasso.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 16:49:06 2019
+
+@author: DongXiaoning
+"""
+
