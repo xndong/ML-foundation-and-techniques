788f71474b3e039bceaf3c440923c5ffb30c418b
Dongxn@NUDT <Dong.Xiaoning@outlook.com>
2019-09-19 23:55:21 +0800

    clustring algorithm(kmeans) and xample application

---
+++ Kmeans/Kmeans.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 23:50:07 2019
+
+@author: DongXiaoning
+"""
+
+"""
+# Location, i.e clustered by the value in this location
+# Shape
+# Density
+"""
---
+++ Kmeans/KmeansImageSegmentation.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 23:50:33 2019
+
+@author: DongXiaoning
+"""
+
