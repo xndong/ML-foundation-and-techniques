e567013668efc470b89b90e85f21af7eb31a5b88
Dongxn@NUDT <Dong.Xiaoning@outlook.com>
2019-09-20 23:12:54 +0800

    Kmeans and pixel clustering

--- Kmeans/Kmeans.py
+++ Kmeans/Kmeans.py
@@ -10,3 +10,74 @@ Created on Thu Sep 19 23:50:07 2019
 # Shape
 # Density
 """
+import numpy as np
+import sklearn
+import os.path
+import sys
+
+def load_data():
+    raw_data_array = np.genfromtxt('cars.csv',delimiter = ',')
+    data = raw_data_array[1:,:-1]
+    return data
+
+# how many k : no rules.
+# initialize k centers: several methods ---> random, distance-based
+def init_centers(k,n):
+    center_list=[]
+    for i in range(k):
+        center = 10*np.random.rand(n,) + 0.5
+        center_list.append(center)
+    return center_list
+def rand_sample_as_initcenter(k,data):
+    m,n = np.shape(data)
+    rows = np.random.randint(0,m,k)
+    centers = data[rows]
+    return centers
+    
+# compute distance between center and data point, several distance metrics
+def compute_distance(center,point,distance_metric='euclidean'):
+    if distance_metric == 'euclidean':
+        return euclidean_distance(center,point)
+    else:
+        return manhattan_distance(center,point)
+    
+# distance metrics, Euclidean and Manhattan Distance as examples
+def euclidean_distance(point_x,point_y):
+    vector = np.subtract(point_x,point_y)
+    distance = np.dot(vector,vector)
+    return distance
+    
+def manhattan_distance(point_x,point_y):
+    vector = np.subtract(point_x,point_y)
+    distance = np.sum(vector)
+    return distance
+
+# compute which cluster the point should belong to
+def choose_cluster():
+    return
+
+# Note that when initial cluster number increase, the total cost can usually decrease. Thus, when doing model selection, ensure that k is the same!
+# Or give a penalty on k ---> what we usually do.
+def total_cost():
+    return
+
+def kmeans():
+    return
+
+def main():
+    data = load_data()
+    m,n = np.shape(data)
+    center_number = 5
+    center_category = ['one','two','three','four','five']
+    distance_list = []
+    centers = rand_sample_as_initcenter(center_number,data)   # center_list = init_centers(center_number,n)
+    for i in range(m):
+        for j in range(center_number):
+            distance = compute_distance(centers[j],data[i])
+            distance_list.append(distance)
+        min_distance = min(distance_list)
+        index = distance_list.
+    return
+
+if __name__ == "__main__":
+    main()
--- Kmeans/KmeansImageSegmentation.py
+++ Kmeans/KmeansImageSegmentation.py
@@ -4,4 +4,75 @@ Created on Thu Sep 19 23:50:33 2019
 
 @author: DongXiaoning
 """
+import numpy as np
+from sklearn.cluster import KMeans
+import PIL.Image as image
+import os.path
+import scipy.cluster.vq
+from scipy.misc import imresize
+import pylab
 
+
+
+FILE_NAME = 'photo.jpg'
+
+def load_image(file_path):
+    with open(file_path,'rb') as rf:
+        img = np.array(image.open(rf))
+    return img
+
+# square = 2, height_steps = 160
+# square = 4, height_steps = 80
+
+def cluster_pix_square(img,k,height_steps):
+    curr = os.getcwd()
+    file_path = os.path.join(curr,FILE_NAME)
+    #img = load_image(file_path)
+    img = np.array(image.open(file_path))
+    height = img.shape[0]
+    width = img.shape[1]
+    square = height / height_steps
+    width_steps = width / square
+    square = int(square)
+    width_steps = int(width_steps)
+    pixels = []
+    for i in range(height_steps):
+        for j in range(width_steps):
+            R = np.mean(img[i*square:(i+1)*square,j*square:(j+1)*square,0])
+            G = np.mean(img[i*square:(i+1)*square,j*square:(j+1)*square,1])
+            B = np.mean(img[i*square:(i+1)*square,j*square:(j+1)*square,2])
+            pixels.append([R,G,B])
+    pixels = np.array(pixels,'f')
+    centroids, variance = scipy.cluster.vq.kmeans(pixels, k)
+    code, distance = scipy.cluster.vq.vq(pixels, centroids)
+    codeim = code.reshape(height_steps, width_steps)
+    codeim = imresize(codeim, img.shape[:2], 'nearest')
+    return codeim
+
+def main():
+    infile = 'photo.jpg'
+    im = np.array(image.open(infile))
+
+    m_k = 15
+
+    pylab.figure()
+    pylab.subplot(131)
+    pylab.title('source')
+    pylab.imshow(im)
+
+    codeim= cluster_pix_square(infile, m_k,160)
+    pylab.subplot(132)
+    pylab.title('Steps = 160, K = '+str(m_k));
+    pylab.imshow(codeim)
+
+    codeim= cluster_pix_square(infile, 5, 160)
+    pylab.subplot(133)
+    pylab.title('Steps = 160, K = 5');
+    pylab.imshow(codeim)
+
+    pylab.show()
+    return
+
+
+if __name__=='__main__':
+    main()
